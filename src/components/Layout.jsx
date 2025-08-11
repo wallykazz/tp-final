d8ceff3152e76518304640cde7167c7b036178dc
wallykazz <wallykazz@gmail.com>
2025-08-10 21:25:42 -0300

    se arreglo problema del posicionamiento del navbar

--- src/components/Layout.jsx
+++ src/components/Layout.jsx
@@ -6,12 +6,12 @@ const Layout = (props) => {
   return (
     <div className={props.background}>
       <Header />
-      <main>
+      <main style={{ paddingTop: "64px" }}>
         {props.children}
       </main>
       <Footer />
     </div>
-  )
-}
+  );
+};
 
-export { Layout }
+export { Layout };
