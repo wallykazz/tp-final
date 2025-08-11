758bbd2c8d605115e77708a091243660744a1c2f
wallykazz <wallykazz@gmail.com>
2025-08-11 18:45:57 -0300

    se estilo el footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,9 +1,23 @@
+import "../styles/components/Footer.css";
+
 const Footer = () => {
   return (
-    <footer style={{ backgroundColor: "lightcoral" }}>
-      <p>Sitio desarrollado por <a href="https://github.com/wallykazz" target="_blank">Walter Perez</a></p>
+    <footer className="main-footer">
+      <p>
+        Sitio desarrollado por{" "}
+        <a href="https://github.com/wallykazz" target="_blank" rel="noopener noreferrer" className="github-link">
+          <span className="github-wrapper">
+            <img
+              src="https://cdn-icons-png.flaticon.com/512/25/25231.png"
+              alt="GitHub Logo"
+              className="github-icon"
+            />
+            Walter Perez
+          </span>
+        </a>
+      </p>
     </footer>
-  )
-}
+  );
+};
 
-export { Footer }
+export { Footer };
