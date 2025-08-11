99e69c2d75206102e9308a25da84d3d206de12f6
wallykazz <wallykazz@gmail.com>
2025-08-11 17:54:02 -0300

    se estilo el login

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -3,6 +3,7 @@ import { useState } from "react"
 import { Layout } from "../components/Layout"
 import { useAuth } from "../context/UserContext"
 import { useNavigate } from "react-router-dom"
+import "../styles/pages/Login.css"
 
 const Login = () => {
   const [username, setUsername] = useState("")
@@ -25,30 +26,34 @@ const Login = () => {
 
   return (
     <Layout>
-      <h1>Inicia sesión</h1>
+      <div className="login-container">
+        <h1>Inicia sesión</h1>
 
-      <section>
-        <h2>Hola, bienvenido de nuevo</h2>
-        <p>Usuario: johnd</p>
-        <p>Contraseña: m38rmF$</p>
-        <form onSubmit={handleLogin}>
-          <div>
-            <label>Nombre de usuario:</label>
-            <input
-              type="text"
-              onChange={(e) => setUsername(e.target.value)}
-              value={username} />
-          </div>
-          <div>
-            <label>Contraseña:</label>
-            <input
-              type="password"
-              onChange={(e) => setPassword(e.target.value)}
-              value={password} />
-          </div>
-          <button>Ingresar</button>
-        </form>
-      </section>
+        <section>
+          <h2>Hola, bienvenido de nuevo.</h2>
+          <p>Usuario: johnd</p>
+          <p>Contraseña: m38rmF$</p>
+          <form onSubmit={handleLogin}>
+            <div>
+              <label>Nombre de usuario:</label>
+              <input
+                type="text"
+                onChange={(e) => setUsername(e.target.value)}
+                value={username}
+              />
+            </div>
+            <div>
+              <label>Contraseña:</label>
+              <input
+                type="password"
+                onChange={(e) => setPassword(e.target.value)}
+                value={password}
+              />
+            </div>
+            <button type="submit">Ingresar</button>
+          </form>
+        </section>
+      </div>
     </Layout>
   )
 }
