dbe73af941f11ca0d2f8682d2fcd8128d576b727
wallykazz <wallykazz@gmail.com>
2025-08-13 19:56:31 -0300

    estilo del footer alineado a los extremos

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -3,19 +3,21 @@ import "../styles/components/Footer.css";
 const Footer = () => {
   return (
     <footer className="main-footer">
-      <p>
-        Sitio desarrollado por{" "}
-        <a href="https://github.com/wallykazz" target="_blank" rel="noopener noreferrer" className="github-link">
-          <span className="github-wrapper">
-            <img
-              src="https://cdn-icons-png.flaticon.com/512/25/25231.png"
-              alt="GitHub Logo"
-              className="github-icon"
-            />
-            Walter Perez
-          </span>
-        </a>
-      </p>
+      <div className="footer-content">
+        <p className="footer-left">© 2025 Walter Perez. Todos los derechos reservados.</p>
+        <p className="footer-right">
+          <a href="https://github.com/wallykazz" target="_blank" rel="noopener noreferrer" className="github-link">
+            <span className="github-wrapper">
+              <img
+                src="https://cdn-icons-png.flaticon.com/512/25/25231.png"
+                alt="GitHub Logo"
+                className="github-icon"
+              />
+              Repositorio en GitHub
+            </span>
+          </a>
+        </p>
+      </div>
     </footer>
   );
 };
